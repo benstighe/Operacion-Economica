8728c4f57abcd7eced7f149aeb13bd4154376bd5
benstighe <benstighe@uc.cl>
2024-04-03 12:06:35 -0300

    archivo principal

--- Tarea1.jl
+++ Tarea1.jl
@@ -1,7 +1,17 @@
 using JuMP, DataFrames, Gurobi, CSV
 model = Model(Gurobi.Optimizer)
 
-@variable(model, 0 <= Pg[id_gen] <= Pmax[id_gen]) #Pg : Cantidad de energía generada [MWh]
+@variable(model, Pmin[id_gen] <= Pg[id_gen] <= Pmax[id_gen]) #Pg : Cantidad de energía generada [MWh]
 @variable(model, Theta[id_bar]) #Theta : angulos de las barras
 
-@constraint(model, Cumplir_demanda, sum(Pg[id_gen] for id_gen in ID_SET) - sum()) 
+for i in barras
+
+    @constraint(model, Cumplir_demanda, sum(Pg[id_gen] for id_gen in ID_SET_gen_in_barra_i) - 
+    sum(B[i,j]*(Theta[i]-Theta[j]) for j in ID_SET_barras_conectadas_a_i) == D[i]) 
+
+    end
+
+for k in lineas
+    @constraint(model, Flujo, -Fmax[k] <= B[[k]]*(Theta[k[0]]-Theta[k[1]] <= Fmax[k]))
+end 
+
