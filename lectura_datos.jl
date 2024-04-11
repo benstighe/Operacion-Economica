7466d354a711b6d600a038daef96d64f653dd451
mbertin1 <mbertin@uc.cl>
2024-04-11 12:34:20 -0400

    mensaje hola

--- lectura_datos.jl
+++ lectura_datos.jl
@@ -4,4 +4,4 @@ demanda = CSV.read("Demand.csv", DataFrame)
 generadores = CSV.read("Generators.csv", DataFrame)
 lineas = CSV.read("Lines.csv", DataFrame)
 bess = CSV.read("Bess.csv", DataFrame)
-
+#hola
