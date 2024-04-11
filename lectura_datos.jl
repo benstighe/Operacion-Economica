622221ddd50b8443c8fdd1905ea55ea876076952
JoaquinCanelo <jcanelopez@uc.cl>
2024-04-11 13:05:28 -0400

    Agregadas definicion de objeto generador

--- lectura_datos.jl
+++ lectura_datos.jl
@@ -1,7 +1,18 @@
+include("structs.jl")
+
 using CSV, DataFrames
 
 demanda = CSV.read("Demand.csv", DataFrame)
 generadores = CSV.read("Generators.csv", DataFrame)
 lineas = CSV.read("Lines.csv", DataFrame)
 bess = CSV.read("Bess.csv", DataFrame)
+
+nombres_columnas = names(generadores)
+
+gen = Generador(generadores[!,1],generadores[!,2],generadores[!,3],generadores[!,4],generadores[!,5],generadores[!,6])
+
+println("ID: ", gen.ID)
+println("Bus: ", gen.Bus_Conexion)
+println("Pmax: ", gen.Pmax)
+println("Pmin: ", gen.Pmin)
 #hola
--- structs.jl
+++ structs.jl
@@ -1,19 +1,19 @@
 struct Generador
     ID :: Vector{Int64}
-    Bus :: Vector{Int64}
+    Pmin :: Vector{Int64}
     Pmax :: Vector{Float64}
-    Pmin :: Vector{Float64}
-    Cfijo :: Vector{Float64}
-    Cvariable :: Vector{Float64}
+    Cvariable :: Vector{Int64}
+    Ramp :: Vector{Int64}
+    Bus_Conexion :: Vector{Int64}
 end
 
 # Creating an instance of Generador
 #gen = Generador([1, 2, 3], [4, 5, 6], [10.0, 20.0, 30.0], [5.0, 10.0, 15.0])
 
-println("ID: ", gen.ID)
-println("Bus: ", gen.Bus[1])
-println("Pmax: ", gen.Pmax[1])
-println("Pmin: ", gen.Pmin[1])
+#println("ID: ", gen.ID)
+#println("Bus: ", gen.Bus[1])
+#println("Pmax: ", gen.Pmax[1])
+#println("Pmin: ", gen.Pmin[1])
 
 struct Loads
     ID :: Vector{Int64}
