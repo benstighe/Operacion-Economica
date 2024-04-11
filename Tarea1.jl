7b2d52c0a9010b22ebbf3e5257086d5f569d0d3b
mbertin1 <mbertin@uc.cl>
2024-04-11 12:52:29 -0400

    puse los t

--- Tarea1.jl
+++ Tarea1.jl
@@ -1,17 +1,20 @@
 using JuMP, DataFrames, Gurobi, CSV
 model = Model(Gurobi.Optimizer)
 
-@variable(model, Pmin[id_gen] <= Pg[id_gen] <= Pmax[id_gen]) #Pg : Cantidad de energía generada [MWh]
-@variable(model, Theta[id_bar]) #Theta : angulos de las barras
+@variable(model, Pmin[id_gen] <= Pg[id_gen,t] <= Pmax[id_gen]) #Pg : Cantidad de energía generada [MWh]
+@variable(model, Theta[id_bar,t]) #Theta : angulos de las barras
 
-for i in barras
+for t in tiempos
+    for i in barras
 
-    @constraint(model, Cumplir_demanda, sum(Pg[id_gen] for id_gen in ID_SET_gen_in_barra_i) - 
-    sum(B[i,j]*(Theta[i]-Theta[j]) for j in ID_SET_barras_conectadas_a_i) == D[i]) 
+        @constraint(model, Cumplir_demanda, sum(Pg[id_gen,t] for id_gen in ID_SET_gen_in_barra_i) - 
+        sum(B[i,j]*(Theta[i,t]-Theta[j,t]) for j in ID_SET_barras_conectadas_a_i) == D[i,t]) 
 
     end
-
-for k in lineas
-    @constraint(model, Flujo, -Fmax[k] <= B[[k]]*(Theta[k[0]]-Theta[k[1]] <= Fmax[k]))
-end 
+end
+for t in tiempos
+    for k in lineas
+        @constraint(model, Flujo, -Fmax[k] <= B[[k]]*(Theta[k[0],t]-Theta[k[1],t] <= Fmax[k]))
+    end 
+end
 
