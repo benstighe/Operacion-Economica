d23f1bb5486854f7584478b65ac7a100f1ff8432
mbertin1 <mbertin@uc.cl>
2024-04-11 13:28:01 -0400

    arreglo p1

--- Tarea1.jl
+++ Tarea1.jl
@@ -1,10 +1,11 @@
 using JuMP, DataFrames, Gurobi, CSV
+include("lectura_datos.jl")
 model = Model(Gurobi.Optimizer)
+tiempo=[1:6]
+@variable(model, Pg[gen.ID,tiempo] ) #Pg : Cantidad de energía generada [MWh]
+@variable(model, Theta[bar.ID,tiempo]) #Theta : angulos de las barras
 
-@variable(model, Pmin[id_gen] <= Pg[id_gen,t] <= Pmax[id_gen]) #Pg : Cantidad de energía generada [MWh]
-@variable(model, Theta[id_bar,t]) #Theta : angulos de las barras
-
-for t in tiempos
+for t in tiempo
     for i in barras
 
         @constraint(model, Cumplir_demanda, sum(Pg[id_gen,t] for id_gen in ID_SET_gen_in_barra_i) - 
@@ -12,9 +13,14 @@ for t in tiempos
 
     end
 end
-for t in tiempos
+for t in tiempo
     for k in lineas
         @constraint(model, Flujo, -Fmax[k] <= B[[k]]*(Theta[k[0],t]-Theta[k[1],t] <= Fmax[k]))
     end 
 end
 
+for t in tiempo
+    for id_gen in gen.ID
+        @constraint(model,Cumplir_potencias,gen.Pmin[id_gen] <= Pg[id_gen,t] <= gen.Pmax[id_gen])
+    end
+end
