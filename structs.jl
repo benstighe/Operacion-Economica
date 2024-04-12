c62083c5d3794b6e4e29f71cde18dd511657b5a2
benstighe <benstighe@uc.cl>
2024-04-12 13:24:51 -0400

    estructuras nuevas para hacer lista de objetos no lista de listas

--- structs.jl
+++ structs.jl
@@ -1,44 +1,34 @@
 struct Generador
-    ID :: Vector{Int64}
-    Pmin :: Vector{Int64}
-    Pmax :: Vector{Float64}
-    Cvariable :: Vector{Int64}
-    Ramp :: Vector{Int64}
-    Bus_Conexion :: Vector{Int64}
+    ID::Int64
+    Pmin::Int64
+    Pmax::Float64
+    Cvariable::Int64
+    Ramp::Int64
+    Bus_Conexion::Int64
 end
 
-# Creating an instance of Generador
-#gen = Generador([1, 2, 3], [4, 5, 6], [10.0, 20.0, 30.0], [5.0, 10.0, 15.0])
-
-#println("ID: ", gen.ID)
-#println("Bus: ", gen.Bus[1])
-#println("Pmax: ", gen.Pmax[1])
-#println("Pmin: ", gen.Pmin[1])
 
 struct Loads
-    ID_Bus :: Vector{Int64}
-    D_t1 :: Vector{Int64}
-    D_t2 :: Vector{Int64}
-    D_t3 :: Vector{Int64}
-    D_t4 :: Vector{Int64}
-    D_t5 :: Vector{Int64}
-    D_t6 :: Vector{Int64}
+    ID_Bus::Int64
+    D_t1::Int64
+    D_t2::Int64
+    D_t3::Int64
+    D_t4::Int64
+    D_t5::Int64
+    D_t6::Int64
 end
 
 struct Lines
-    ID :: Vector{Int64}
-    FromBus :: Vector{Int64}
-    ToBus :: Vector{Int64}
-    Fmax :: Vector{Int64}
-    X :: Vector{Float64}
-    
+    ID::Int64
+    FromBus::Int64
+    ToBus::Int64
+    Fmax::Int64
+    X::Float64
 end
 
 struct Bess
-    ID :: Vector{Int64}
-    Cap :: Vector{Int64}
-    Rend :: Vector{Float64}
-    Bus_conexion :: Vector{Int64}
+    ID::Int64
+    Cap::Int64
+    Rend::Float64
+    Bus_conexion::Int64
 end
-
-
