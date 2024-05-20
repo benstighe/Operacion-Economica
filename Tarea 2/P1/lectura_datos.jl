203295fb6a28052254a5a41b271e7b475b6c4e67
JoaquinCanelo <jcanelopez@uc.cl>
2024-05-20 12:27:09 -0400

    lectura demanda

--- Tarea 2/P1/lectura_datos.jl
+++ Tarea 2/P1/lectura_datos.jl
@@ -1,10 +1,25 @@
 using XLSX, Statistics
 
-caso_014 = XLSX.readxlsx("Case014.xlsx")
+caso_014 = XLSX.readxlsx("Tarea 2/P1/Case014.xlsx")
 
 Buses = caso_014["Buses"]
 Demand = caso_014["Demand"]
 Generators = caso_014["Generators"]
 Lines = caso_014["Lines"]
 Renewables = caso_014["Renewables"]
+
+lectura_demanda = Demand["B3:Y16"]
+
+
+demanda = []
+
+for j in 1:14
+    lista_barra = []
+    for i in 1:24
+        push!(lista_barra, lectura_demanda[14*(i-1)+j])
+    end
+    push!(demanda, lista_barra)
+end
+
+println(demanda[14])
 
