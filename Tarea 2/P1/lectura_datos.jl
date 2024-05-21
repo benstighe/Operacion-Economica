d807c07344003300a032e787f2c57b8e61947978
JoaquinCanelo <jcanelopez@uc.cl>
2024-05-21 12:35:22 -0400

    agregado algunas cosas

--- Tarea 2/P1/lectura_datos.jl
+++ Tarea 2/P1/lectura_datos.jl
@@ -1,4 +1,5 @@
 using XLSX, Statistics
+include("Tarea 2/P1/estructuras.jl")
 
 caso_014 = XLSX.readxlsx("Tarea 2/P1/Case014.xlsx")
 
@@ -7,6 +8,8 @@ Demand = caso_014["Demand"]
 Generators = caso_014["Generators"]
 Lines = caso_014["Lines"]
 Renewables = caso_014["Renewables"]
+
+#------ LECTURA DE DEMANDA ----------#
 
 lectura_demanda = Demand["B3:Y16"]
 
@@ -17,8 +20,10 @@ for j in 1:14
     for i in 1:24
         push!(lista_barra, lectura_demanda[14*(i-1)+j])
     end
-    push!(demanda, lista_barra)
+    push!(loads_list, Loads(j, lista_barra))
 end
 
-println(demanda[14])
-
+# Imprimir las instancias creadas para verificar
+for load in loads_list
+    println(load)
+end
