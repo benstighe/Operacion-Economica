abf34c0082d83400dd2863be3d253f6ca9009eb6
benstighe <benstighe@uc.cl>
2024-05-23 17:34:16 -0400

    montecarlo v3

--- Tarea 2/P2/montecarlo.jl
+++ Tarea 2/P2/montecarlo.jl
@@ -14,15 +14,32 @@ include("lectura_datos118.jl")
 
 
 #println(lectura_ren_generacion[1,2]) #(gen,hora)
-dev_estandar_eolico = lectura_ren_generacion
+dev_estandar_eolico = lectura_ren_generacion[1:40, 1:24]
+dev_estandar_solar = lectura_ren_generacion[41:60, 1:24]
 #eolico
 for i in 1:40 
     for t in 1:24
         dev_estandar_eolico[i,t] = lectura_ren_generacion[i,t]*κ_t_eolico_array[t]
     end
 end
-println(dev_estandar_eolico)
-# Imprimir los valores de κ_t
-println("κ_t_eolico: ", κ_t_eolico_array)
-#println("κ_t_solar: ", κ_t_solar_array)
+#solar
+for i in 1:20 
+    for t in 1:24
+        dev_estandar_solar[i,t] = lectura_ren_generacion[i,t]*κ_t_solar_array[t]
+    end
+end
 
+
+#eolico
+for j in 1:40
+    for t in 1:24
+        dist = Normal(0,dev_estandar_eolico[j,t])
+        x = rand(dist,1)
+        println(x)
+    end 
+end 
+
+
+
+
+#solar
