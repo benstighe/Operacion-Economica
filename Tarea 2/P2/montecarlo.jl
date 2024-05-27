18049150e4baf3fb26929dabf0352b00af92a654
mbertin1 <mbertin@uc.cl>
2024-05-27 12:02:40 -0400

    monte

--- Tarea 2/P2/montecarlo.jl
+++ Tarea 2/P2/montecarlo.jl
@@ -34,28 +34,35 @@ end
 eolico_pronostico = lectura_ren_generacion[1:40, 1:24]
 solar_pronostico = lectura_ren_generacion[41:60, 1:24]
 
-
+eolico_montecarlo=[]
+solar_montecarlo=[]
+#finalmente cada lista tiene cada simulacion para todos los generadores en cada tiempo
+suma_eolico_montecarlo=[]#arreglar que sea para cada t
+suma_solar_montecarlo=[]
 for semilla in 1:100
     Random.seed!(semilla)
     #eolico
+    suma_eolico=0
     for j in 1:40
         for t in 1:24
             dist = Normal(0, dev_estandar_eolico[j,t])
             epsilon = rand(dist, 1)
             global eolico_pronostico[j,t] = max(0.0, lectura_ren_generacion[j,t] + epsilon[1])
+            suma_eolico=suma_eolico+eolico_pronostico[j,t]
         end 
     end 
-
-    println(eolico_pronostico)
-
+    push!(eolico_montecarlo, eolico_pronostico)
+    push!(suma_eolico_montecarlo, suma_eolico)
     #solar
+    suma_solar=0
     for j in 1:20
         for t in 6:19
             dist = Normal(0, dev_estandar_solar[j,t])
             epsilon = rand(dist, 1)
             global solar_pronostico[j,t] = max(0.0, lectura_ren_generacion[j+40,t] + epsilon[1])
+            suma_solar=suma_solar+solar_pronostico[j,t]
         end 
     end 
-    vcat(eolico_pronostico,solar_pronostico)
-    println(eolico_pronostico)
+    push!(solar_montecarlo, solar_pronostico)
+    push!(suma_solar_montecarlo, suma_solar)
 end
